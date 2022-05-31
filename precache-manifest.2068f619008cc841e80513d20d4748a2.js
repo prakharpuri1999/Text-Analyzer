7acde828e77f27433ba9cda2f8ca9f064cd6272e
Prakhar Puri <prakharpuri1999@gmail.com>
2022-05-31 12:29:26 +0530

    Updates

---
+++ precache-manifest.2068f619008cc841e80513d20d4748a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a909b5aea2166e5b887",
+    "url": "/Text-Analyzer/static/css/main.196e015f.chunk.css"
+  },
+  {
+    "revision": "4a909b5aea2166e5b887",
+    "url": "/Text-Analyzer/static/js/main.4a909b5a.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/Text-Analyzer/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/Text-Analyzer/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "fa4f426ec6102caef9a4",
+    "url": "/Text-Analyzer/static/js/runtime~main.fa4f426e.js"
+  },
+  {
+    "revision": "7f27df5d078c4002f5a230c0f386507c",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
